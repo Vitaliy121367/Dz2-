c727023b66c54ce76eadceea45c547b92ace85c2
Vitaliy <qw929496@gmail.com>
2025-05-24 18:08:50 +0300

    Thread

--- Dz2/Program.cs
+++ Dz2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Dz2
 {
@@ -7,19 +8,79 @@ namespace Dz2
         static void Main()
         {
             AppDomain domain = AppDomain.CreateDomain("ShopVinylsDomain");
-
             var service = (MarshalByRefObject)domain.CreateInstanceAndUnwrap(
                 "ShopVinyls",
-                "ShopVinyls.ServiceVinyl"); 
+                "ShopVinyls.ServiceVinyl");
 
             dynamic vinylService = service;
 
-            var vinyls = vinylService.GetAllVinyls();
-            foreach (var vinyl in vinyls)
+            Thread createVinyl = new Thread(() =>
             {
-                Console.WriteLine(
-                        $"ID: {vinyl.Id}, Title: {vinyl.Title}, Artist: {vinyl.Artist}, Publisher: {vinyl.Publisher}, Tracks: {vinyl.TrackCount}, Genre: {vinyl.Genre}, Year: {vinyl.ReleaseYear}, CostPrice: {vinyl.CostPrice}, SalePrice: {vinyl.SalePrice}, IsOnSale: {vinyl.IsOnSale}, Discount: {vinyl.DiscountPercentage}%, SoldCount: {vinyl.SoldCount}, Reserved: {vinyl.IsReserved}, ReservedByUserId: {vinyl.ReservedByUserId}");
-            }
+                var vinyl = new Vinyl
+                {
+                    Id = 12,
+                    Title = "The Dark Side of the Moon",
+                    Artist = "Pink Floyd",
+                    Publisher = "Harvest Records",
+                    TrackCount = 10,
+                    Genre = "Progressive Rock",
+                    ReleaseYear = 1973,
+                    CostPrice = 15.99m,
+                    SalePrice = 24.99m,
+                    IsOnSale = true,
+                    DiscountPercentage = 20,
+                    SoldCount = 5000,
+                    IsReserved = true,
+                    ReservedByUserId = 42
+                };
+                vinylService.AddVinyl(vinyl);
+            });
+            Thread readVinyls = new Thread(() =>
+            {
+                var vinyls = vinylService.GetAllVinyls();
+                foreach (var vinyl in vinyls)
+                {
+                    Console.WriteLine(
+                            $"ID: {vinyl.Id}, Title: {vinyl.Title}, Artist: {vinyl.Artist}, Publisher: {vinyl.Publisher}, Tracks: {vinyl.TrackCount}, Genre: {vinyl.Genre}, Year: {vinyl.ReleaseYear}, CostPrice: {vinyl.CostPrice}, SalePrice: {vinyl.SalePrice}, IsOnSale: {vinyl.IsOnSale}, Discount: {vinyl.DiscountPercentage}%, SoldCount: {vinyl.SoldCount}, Reserved: {vinyl.IsReserved}, ReservedByUserId: {vinyl.ReservedByUserId}");
+                }
+            });
+
+            Thread updateVinyl = new Thread(() =>
+            {
+                var vinyl = new Vinyl
+                {
+                    Id = 1,
+                    Title = "The Dark Side of the Moon",
+                    Artist = "Pink Floyd",
+                    Publisher = "Harvest Records",
+                    TrackCount = 10,
+                    Genre = "Progressive Rock",
+                    ReleaseYear = 1973,
+                    CostPrice = 15.99m,
+                    SalePrice = 24.99m,
+                    IsOnSale = true,
+                    DiscountPercentage = 20,
+                    SoldCount = 5000,
+                    IsReserved = true,
+                    ReservedByUserId = 42
+                };
+                vinylService.UpdateVinyl(vinyl);
+            });
+
+            Thread deleteThread = new Thread(() =>
+            {
+                vinylService.DeleteVinyl(11);
+            });
+
+            createVinyl.Start();
+            updateVinyl.Start();
+            deleteThread.Start();
+            readVinyls.Start();
+
+            createVinyl.Join();
+            updateVinyl.Join();
+            deleteThread.Join();
+            readVinyls.Join();
 
             Console.ReadKey();
         }
---
+++ Dz2/Vinyls.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz2
+{
+    public class Vinyl
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Publisher { get; set; }
+        public int TrackCount { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public decimal CostPrice { get; set; }
+        public decimal SalePrice { get; set; }
+        public bool IsOnSale { get; set; }
+        public int DiscountPercentage { get; set; }
+        public int SoldCount { get; set; }
+        public bool IsReserved { get; set; }
+        public int? ReservedByUserId { get; set; }
+    }
+
+}
